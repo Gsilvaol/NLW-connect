bb326b6bd123c8b415b83ba5cc7b86df8a049e80
Gustavo <gustavosilva1994_@hotmail.com>
2025-02-21 12:14:47 -0300

    Aula 02 - Integração com banco de dados e primeiras funcionalidades

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,10 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const envShema = z.object({
-    PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
+  WEB_URL: z.string().url(),
 })
 
-export const env = envShema.parse(process.env);
+export const env = envShema.parse(process.env)
